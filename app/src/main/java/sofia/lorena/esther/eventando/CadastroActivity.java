be506b062e79f606115f73b308431864b5f9c236
20211tiimi0222 <lorenatoraessantos@gmail.com>
2023-06-07 09:32:17 -0300

    Tela de cadastro iniciada

--- app/src/main/java/sofia/lorena/esther/eventando/CadastroActivity.java
+++ app/src/main/java/sofia/lorena/esther/eventando/CadastroActivity.java
@@ -1,4 +1,14 @@
 package sofia.lorena.esther.eventando;
 
-public class CadastroActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
 }

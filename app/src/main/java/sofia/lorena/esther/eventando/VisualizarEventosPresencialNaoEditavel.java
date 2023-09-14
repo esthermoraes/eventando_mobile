2fd2de6407f7a23a326469441ac17b1545e8a190
20211TIIMI0273 <andradesoso16@gmail.com>
2023-09-14 10:36:54 -0300

    eita

---
+++ app/src/main/java/sofia/lorena/esther/eventando/VisualizarEventosPresencialNaoEditavel.java
@@ -0,0 +1,14 @@
+package sofia.lorena.esther.eventando;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VisualizarEventosPresencialNaoEditavel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualizar_eventos_presencial_nao_editavel);
+    }
+}
